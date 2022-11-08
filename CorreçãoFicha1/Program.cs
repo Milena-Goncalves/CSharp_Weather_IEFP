bd02a61dd6c6c842a6e6fae088bbc149aa5720b6
JoaoMonge <joao.monge13@gmail.com>
2022-11-08 11:46:44 +0000

    Initial commit

---
+++ CorreçãoFicha1/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+using Newtonsoft.Json;
+
+
+var client = new HttpClient();
+var request = new HttpRequestMessage
+{
+    Method = HttpMethod.Get,
+    RequestUri = new Uri("https://covid-193.p.rapidapi.com/statistics?country=Portugal"),
+    Headers =
+    {
+        { "X-RapidAPI-Key", "2c919a96d7msh4b3c8f542ec9108p1b1e5fjsn5c0f938ee222" },
+        { "X-RapidAPI-Host", "covid-193.p.rapidapi.com" },
+    },
+};
+
+using (var response = await client.SendAsync(request))
+{
+    response.EnsureSuccessStatusCode();
+    var body = await response.Content.ReadAsStringAsync();
+
+    dynamic json = JsonConvert.DeserializeObject(body);
+
+    //População
+    Console.WriteLine($"População: {json["response"][0]["population"]} habitantes.");
+
+
+    //Total casos
+    Console.WriteLine($"Casos: {json["response"][0]["cases"]["total"]}.");
+
+}
